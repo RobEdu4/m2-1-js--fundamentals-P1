6f4d5e00d61edaee4a2cb34df80f3f4422218467
RobertPanneton <robert.panneton@gmail.com>
2020-09-24 00:12:51 -0400

    ex11-lesslines

--- __workshop/exercise-10.js
+++ __workshop/exercise-10.js
@@ -12,8 +12,16 @@ function exercise10() {
   // #######
   //
   // Write code between the lines (below)
-  // -----------------------------------------------------------------
-  // -----------------------------------------------------------------
+
+for (let i = 1; i < 8; i++){
+  let poundLayout = "";
+  for (let j=0; j < i; j++){
+    poundLayout.push("#");
+  }
+  console.log(poundLayout);
+}
+
+
   // Write code between the lines (above)
   //
   //
